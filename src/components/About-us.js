4dee778b52842838fe6c9af08cf7339226492f69
givenmphiwe <giggslive52@gmail.com>
2023-05-17 10:32:06 +0200

    abt-us

--- src/components/About-us.js
+++ src/components/About-us.js
@@ -11,12 +11,12 @@ import {
   FaPhone,
   FaTwitter,
 } from "react-icons/fa";
-import Lichelle from "../assets/people/lichelle_mpofu.jpg"
-import Andrew from "../assets/people/andrew mkhumeli ngwenya.jpg"
+import Lichelle from "../assets/people/lichelle_mpofu.jpg";
+import Andrew from "../assets/people/andrew mkhumeli ngwenya.jpg";
 
 export const About = () => {
   const navigate = useNavigate();
-  
+
   return (
     <body>
       <div className="heading">
@@ -44,7 +44,12 @@ export const About = () => {
             the next level.
           </p>
 
-          <button className="contact-us-btn" onClick={() => navigate("/Contact")}>Contact Us</button>
+          <button
+            className="contact-us-btn"
+            onClick={() => navigate("/Contact")}
+          >
+            Contact Us
+          </button>
         </div>
       </section>
 
@@ -53,7 +58,10 @@ export const About = () => {
           <div className="row">
             <div className="title">
               <h1>Our team</h1>
-              <p>There are no secrets to sucess,It is the results of preparation, hard work,and learning from setbacks</p>
+              <p>
+                There are no secrets to sucess,It is the results of preparation,
+                hard work,and learning from setbacks
+              </p>
             </div>
           </div>
 
@@ -65,7 +73,10 @@ export const About = () => {
               <div className="content-1">
                 <h3>Lichelle Mpofu</h3>
                 <h2>Founder and CEO</h2>
-                <p>The strength in our team and our ability to deal with adversities is what makes us a great company</p>
+                <p>
+                  The strength in our team and our ability to deal with
+                  adversities is what makes us a great company
+                </p>
               </div>
             </div>
 
@@ -76,9 +87,11 @@ export const About = () => {
               <div className="content-1">
                 <h3>Collen Dutch Maje</h3>
                 <h2>Operations Manager</h2>
-                <p>We aim to be No.1 comapany in client satisfaction by striving for the best Always</p>
+                <p>
+                  We aim to be No.1 comapany in client satisfaction by striving
+                  for the best Always
+                </p>
               </div>
-
             </div>
 
             <div className="card">
@@ -88,19 +101,21 @@ export const About = () => {
               <div className="content-1">
                 <h3>Andrew Mkhulumeli Ngwenya</h3>
                 <h2>Business Developer</h2>
-                <p>We will accelerate our business activities to safeguard Our clients and staff</p>
+                <p>
+                  We will accelerate our business activities to safeguard Our
+                  clients and staff
+                </p>
               </div>
-
             </div>
           </div>
         </div>
       </div>
 
       <footer className="footer-1">
-          <div className="row">
-            <div className="col">
-              <img src={Logo}/>
-              <p>Styles Services Group</p>
+        <div className="row">
+          <div className="col">
+            <img src={Logo} />
+            <p>Styles Services Group</p>
             <p>Hospitality solutions</p>
           </div>
 
@@ -108,52 +123,63 @@ export const About = () => {
             <h3>Office</h3>
             <p>325 Rivonia Boulevard,</p>
             <p>Edenburg, Sandton</p>
-            <p className="email-id"><FaEnvelope/>  info@stylesservices.com</p>
-            <h4><FaPhone/> +27 84 602 3144</h4>
+            <p className="email-id">
+              <FaEnvelope /> info@stylesservices.com
+            </p>
+            <h4>
+              <FaPhone /> +27 84 602 3144
+            </h4>
           </div>
           <div className="col">
             <h3>Links</h3>
             <ul>
-              <li className="link-s"><a onClick={() => navigate("/")}>Home</a></li>
-              <li className="link-s"><a onClick={() => navigate("/Service")}>What we do</a></li>
-              <li className="link-s"><a onClick={() => navigate("/Training")}>Training</a></li>
-              <li className="link-s"><a onClick={() => navigate("/Contact")}>Contact us</a></li>
+              <li className="link-s">
+                <a onClick={() => navigate("/")}>Home</a>
+              </li>
+              <li className="link-s">
+                <a onClick={() => navigate("/Service")}>What we do</a>
+              </li>
+              <li className="link-s">
+                <a onClick={() => navigate("/Training")}>Training</a>
+              </li>
+              <li className="link-s">
+                <a onClick={() => navigate("/Contact")}>Contact us</a>
+              </li>
             </ul>
           </div>
           <div className="col">
             <h3>Social media</h3>
             <div className="social-icons">
-              
-            <a
-                  href="https://www.facebook.com/stylesservicesgroup"
-                  className="fab"
-                >
-                  <FaFacebook />
-                </a>
-                <a
-                  href="https://www.linkedin.com/company/stylesservicesgroup/"
-                  className="fab"
-                >
-                  <FaLinkedin />
-                </a>
-                <a
-                  href="https://instagram.com/stylesservicesgroup/"
-                  className="fab"
-                >
-                  <FaInstagram />
-                </a>
-                <a href="https://twitter.com/styles_services" className="fab">
-                  <FaTwitter />
-                </a>
-              
+              <a
+                href="https://www.facebook.com/stylesservicesgroup"
+                className="fab"
+              >
+                <FaFacebook />
+              </a>
+              <a
+                href="https://www.linkedin.com/company/stylesservicesgroup/"
+                className="fab"
+              >
+                <FaLinkedin />
+              </a>
+              <a
+                href="https://instagram.com/stylesservicesgroup/"
+                className="fab"
+              >
+                <FaInstagram />
+              </a>
+              <a href="https://twitter.com/styles_services" className="fab">
+                <FaTwitter />
+              </a>
             </div>
           </div>
-          </div>
-          <hr/>
-          <p className="copyright">
-          Styles Services Group &copy; 2023 - All Rights Reserved | Designed by <a href="">Page Financial Service</a>
+        </div>
+        <hr />
+        <p className="copyright">
+          Styles Services Group &copy; 2023 - All Rights Reserved | Designed by{" "}
+          <a href="">Page Financial Service</a>
         </p>
-        </footer>
+      </footer>
     </body>
   );
 };
