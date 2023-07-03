c73bad6597dd9e0ead5029901ac038953c9d254a
givenmphiwe <giggslive52@gmail.com>
2023-07-03 16:23:55 +0200

    done for monday unpaid

---
+++ src/components/TrainingComponents/Waiter/waiter-four.js
@@ -0,0 +1,53 @@
+import React from "react";
+import data from "../Data/Waiter.json";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import Waiter from "../assets/waiter.jpg";
+import { useNavigate } from "react-router-dom";
+
+
+export const WaiterTwo = () => {
+const navigate = useNavigate();
+  
+  return (
+    <div className="sect">
+      <div className="containa">
+        <div className="content-sect">
+          <div className="titl">
+            <h1>Waiter Training</h1>
+          </div>
+
+          <div className="contety">
+            <div className="image-sec">
+              <img src={Waiter} />
+            </div>
+            <h3>Presenting the menu</h3>
+            
+            <ul>
+              {data.menuStrings?.map((item, i) => {
+                return <p className="list-items" key={i}>{item}</p>;
+              })}
+            </ul>
+
+            <div className="strt-btn">
+              <a onClick={() => navigate("/Waiter-three")}>Next</a>
+            </div>
+            <div className="strt-btn">
+              <a onClick={() => navigate("/Waiter-one")}>Previous</a>
+            </div>
+          </div>
+          <div className="icons-medi">
+            <a href="https://www.facebook.com/stylesservicesgroup">
+              <FaFacebook />
+            </a>
+            <a href="https://twitter.com/styles_services">
+              <FaTwitter />
+            </a>
+            <a href="https://instagram.com/stylesservicesgroup/">
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
