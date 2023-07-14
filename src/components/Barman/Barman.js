adfd8636e52bf7c1aa2bfdcb82e571344cbaafb0
givenmphiwe <giggslive52@gmail.com>
2023-07-14 11:05:40 +0200

    updates

--- src/components/Barman/Barman.js
+++ src/components/Barman/Barman.js
@@ -8,7 +8,7 @@ export const BarmanOne = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="sectionseight">
+    <div className="sectionsONE">
       <div className="containa">
         <div className="content-sect">
           <div className="titl">
